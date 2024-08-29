7c7e046d29eb56c4039815498872327295e021a5
JoelsonTravassos <joelson.travassos@gmail.com>
2024-08-29 01:25:03 -0300

    Using the Lenght method

--- CsharpBookForBeginners/Cap3/TextMethods.cs
+++ CsharpBookForBeginners/Cap3/TextMethods.cs
@@ -13,6 +13,12 @@ namespace CsharpBookForBeginners
 
             WriteLine("TRIM - remove the empty spaces before and after the expression");
             WriteLine($"Name without spaces: {company.Trim()}");
+
+            WriteLine("Lenght - returns the number of characters");
+            WriteLine($"Lenght of the Text: {company.Length}");
+
+            company = company.Trim();
+            WriteLine($"Number of chars after trim(): {company.Length}");
         }
     }
 }
